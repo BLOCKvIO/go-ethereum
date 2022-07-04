64640a0f89d7d92c6e3a28299d5e6db4bc63f489
ww <wojtanowski.w@gmail.com>
2022-07-04 13:17:09 +0200

    enforce base fee

--- consensus/misc/eip1559.go
+++ consensus/misc/eip1559.go
@@ -51,8 +51,17 @@ func VerifyEip1559Header(config *params.ChainConfig, parent, header *types.Heade
 	return nil
 }
 
-// CalcBaseFee calculates the basefee of the header.
 func CalcBaseFee(config *params.ChainConfig, parent *types.Header) *big.Int {
+	v := CalcBaseFeeW(config, parent)
+	if v.Cmp(big.NewInt(10000000000000)) < 0 {
+		return big.NewInt(10000000000000)
+	}
+
+	return v
+}
+
+// CalcBaseFee calculates the basefee of the header.
+func CalcBaseFeeW(config *params.ChainConfig, parent *types.Header) *big.Int {
 	// If the current block is the first EIP-1559 block, return the InitialBaseFee.
 	if !config.IsLondon(parent.Number) {
 		return new(big.Int).SetUint64(params.InitialBaseFee)
