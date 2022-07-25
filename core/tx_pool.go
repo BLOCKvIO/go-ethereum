de7db7b5630c6b314c22a16daeb8afc0a797952c
ww <wojtanowski.w@gmail.com>
2022-07-25 12:58:40 +0200

    .

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -622,7 +622,7 @@ func (pool *TxPool) validateTx(tx *types.Transaction, local bool) error {
 		return ErrInvalidSender
 	}
 	// Drop non-local transactions under our own minimal accepted gas price or tip
-	if tx.GasTipCapIntCmp(pool.gasPrice) < 0 {
+	if tx.GasPrice().Cmp(pool.gasPrice) < 0 {
 		return ErrUnderpriced
 	}
 	// Ensure the transaction adheres to nonce ordering
